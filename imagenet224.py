c248148482c4f8e37ac95a8d998ff745654b3a60
Brian Crafton <crafton.b@husky.neu.edu>
2020-06-25 13:20:57 -0400

    setting up loss function

--- imagenet224.py
+++ imagenet224.py
@@ -20,6 +20,59 @@ tf.config.experimental.set_memory_growth(gpu, True)
 
 ####################################
 
+'''
+x abscissa of the top left corner in pixels
+y ordinate of the top left corner in pixels
+w width of the boxes in pixel
+h height of the boxes in pixel
+ts timestamp of the box in the sequence in microseconds
+class_id 0 for cars and 1 for pedestrians
+'''
+
+# (49499999, 218., 84., 96., 91., 0, 1., 3758)
+# (49499999, 93., 84., 12., 43., 1, 1., 3759)
+
+'''
+ts timestamp of the box in the sequence in microseconds
+x abscissa of the top left corner in pixels
+y ordinate of the top left corner in pixels
+w width of the boxes in pixel
+h height of the boxes in pixel
+class_id 0 for cars and 1 for pedestrians
+obj (0,1)
+[3758, 3759] ???
+'''
+
+def create_labels(dets):
+
+    ndets = len(dets)
+    coords  = np.zeros(shape=[ndets, 7, 7, 5])
+    obj     = np.zeros(shape=[ndets, 7, 7])
+    no_obj  = np.ones(shape=[ndets, 7, 7])
+    cats    = np.zeros(shape=[ndets, 7, 7])
+    
+    for idx in range(ndets):
+        _, x, y, w, h, cat, _, _ = dets[idx]
+        x = x + 0.5 * w
+        y = y + 0.5 * h
+
+        xc = int(x) // 64
+        yc = int(y) // 64
+        
+        x = (x - xc * 48.) / 48. # might want to clip this to zero
+        y = (y - yc * 48.) / 48. # might want to clip this to zero
+        w = w / 288.
+        h = h / 240.
+        
+        coords[idx, xc, yc, :] = np.array([x, y, w, h, 1.])
+        obj[idx, xc, yc] = 1.
+        no_obj[idx, xc, yc] = 0.
+        cats[idx, xc, yc] = cat
+        
+    return coords, obj, no_obj, cats
+
+####################################
+
 # load weights, hope the weight id matches up.
 weights = np.load('resnet18.npy', allow_pickle=True).item()
 
@@ -54,13 +107,11 @@ optimizer = tf.keras.optimizers.Adam(learning_rate=1e-3, beta_1=0.9, beta_2=0.99
 
 def gradients(model, x, y):
     with tf.GradientTape() as tape:
-        pred_logits = model.train(x)
-        pred_label = tf.argmax(pred_logits, axis=1)
-        loss = tf.reduce_mean(tf.nn.sparse_softmax_cross_entropy_with_logits(labels=y, logits=pred_logits))
-        correct = tf.reduce_sum(tf.cast(tf.equal(pred_label, y), tf.float32))
+        out = model.train(x)
+        loss = yolo_loss(out, )
     
     grad = tape.gradient(loss, params)
-    return loss, correct, grad
+    return loss, grad
 
 ####################################
 
@@ -81,24 +132,25 @@ def collect(model, x, y):
 ####################################
 
 def run_train():
-
+    '''
     total = 100
     total_correct = 0
     total_loss = 0
     batch_size = 1
-
+    '''
+    
     # load = Loader('', total // batch_size, batch_size, 8)
     load = np.load('dataset.npy', allow_pickle=True).item()
     xs, ys = load['x'], load['y']
     
     start = time.time()
 
-    for batch in range(0, total, batch_size):
+    for ex in range(len(xs)):
         # while load.empty(): pass # print ('empty')
         
         # x, y = load.pop()
-        s = batch; e = s + batch_size
-        x = xs[s:e].astype(np.float32); y = ys[s:e]
+        x = np.expand_dims(xs[ex].astype(np.float32), axis=0)
+        y = create_labels(ys[ex])
         
         pred = predict(model, x, y)
         
