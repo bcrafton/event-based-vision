0f3569827113cd7674660d2f9823d46022c66fca
Brian Crafton <crafton.b@husky.neu.edu>
2020-06-28 15:41:53 -0400

    not sure what these are

---
+++ train1.py
@@ -0,0 +1,138 @@
+
+import argparse
+import os
+import sys
+
+parser = argparse.ArgumentParser()
+parser.add_argument('--epochs', type=int, default=25)
+# parser.add_argument('--batch_size', type=int, default=50)
+parser.add_argument('--lr', type=float, default=1e-3)
+parser.add_argument('--gpu', type=int, default=0)
+# parser.add_argument('--name', type=str, default="imagenet_weights")
+args = parser.parse_args()
+
+name = '%d_%f.results' % (args.gpu, args.lr)
+
+####################################
+
+import numpy as np
+import tensorflow as tf
+from layers import *
+import time
+import matplotlib.pyplot as plt
+
+####################################
+
+'''
+gpu_devices = tf.config.experimental.list_physical_devices('GPU')
+for device in gpu_devices:
+    tf.config.experimental.set_memory_growth(device, True)
+'''
+
+'''
+gpus = tf.config.experimental.list_physical_devices('GPU')
+gpu = gpus[args.gpu]
+tf.config.experimental.set_visible_devices(gpu, 'GPU')
+tf.config.experimental.set_memory_growth(gpu, True)
+'''
+
+from yolo_loss import yolo_loss
+from draw_boxes import draw_box
+
+####################################
+
+# load weights, hope the weight id matches up.
+weights = np.load('resnet18.npy', allow_pickle=True).item()
+
+# 240, 288
+model = model(layers=[
+conv_block((5,5,12,64), 3), # 80, 96
+
+res_block1(64,   64, 1, weights=weights), # 80, 96
+res_block1(64,   64, 1, weights=weights), # 80, 96
+
+res_block2(64,   128, 2, weights=weights), # 40, 48
+res_block1(128,  128, 1, weights=weights), # 40, 48
+
+res_block2(128,  256, 2, weights=weights), # 20, 24
+res_block1(256,  256, 1, weights=weights), # 20, 24
+
+res_block2(256,  512, 2, weights=weights), # 10, 12
+res_block1(512,  512, 1, weights=weights), # 10, 12
+
+res_block2(512,  512, 2, weights=None), # 5, 6
+res_block1(512,  512, 1, weights=None), # 5, 6
+
+dense_block(5*6*512, 1024, weights=None),
+dense_block(1024, 5*6*12, weights=None),
+])
+
+params = model.get_params()
+
+####################################
+
+optimizer = tf.keras.optimizers.Adam(learning_rate=args.lr, beta_1=0.9, beta_2=0.999, epsilon=1.)
+
+def gradients(model, x, coord, obj, no_obj, cat, vld):
+    with tf.GradientTape() as tape:
+        out = model.train(x)
+        out = tf.reshape(out, (8, 5, 6, 12))
+        loss = yolo_loss(out, coord, obj, no_obj, cat, vld)
+    
+    grad = tape.gradient(loss, params)
+    return out, loss, grad
+
+####################################
+
+def write(filename, text):
+    print (text)
+    f = open(filename, "a")
+    f.write(text + "\n")
+    f.close()
+
+####################################
+
+from load import Loader
+
+nthread = 4
+batch_size = 8
+nbatch = 100
+
+load = Loader('/home/brian/Desktop/event-based-vision/dataset/data', nbatch, batch_size, nthread)
+
+####################################
+
+total_loss = 0
+total = 0
+
+start = time.time()
+
+for i in range(nbatch):
+    while load.empty(): pass 
+    x, coord, obj, no_obj, cat, vld = load.pop()
+
+    out, loss, grad = gradients(model, x, coord, obj, no_obj, cat, vld)
+    optimizer.apply_gradients(zip(grad, params))
+
+    total_loss += loss.numpy()
+    total += batch_size
+    
+avg_loss = total_loss / total
+avg_rate = total / (time.time() - start)
+load.join()
+
+####################################
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ yolo_loss1.py
@@ -0,0 +1,145 @@
+
+import numpy as np
+import tensorflow as tf
+
+offset_np = [
+[  [0, 0],   [0, 48],   [0, 96],   [0, 144],   [0, 192],   [0, 240]], 
+[ [48, 0],  [48, 48],  [48, 96],  [48, 144],  [48, 192],  [48, 240]], 
+[ [96, 0],  [96, 48],  [96, 96],  [96, 144],  [96, 192],  [96, 240]], 
+[[144, 0], [144, 48], [144, 96], [144, 144], [144, 192], [144, 240]], 
+[[192, 0], [192, 48], [192, 96], [192, 144], [192, 192], [192, 240]]
+]
+
+offset = tf.constant(offset_np, dtype=tf.float32)
+
+def grid_to_pix(box):
+    pix_box_yx = 48. * box[:, :, :, :, 0:2] + offset
+    pix_box_h = box[:, :, :, :, 2:3] * 240. 
+    pix_box_w = box[:, :, :, :, 3:4] * 288.
+    pix_box = tf.concat((pix_box_yx, pix_box_h, pix_box_w), axis=4)
+    return pix_box
+
+def calc_iou(boxA, boxB, realBox):
+    iou1 = calc_iou_help(boxA, realBox)
+    iou2 = calc_iou_help(boxB, realBox)
+    return tf.stack([iou1, iou2], axis=4)
+
+def calc_iou_help(boxA, boxB):
+    # determine the (x, y)-coordinates of the intersection rectangle
+    yA = tf.maximum(boxA[:,:,:,:,0] - 0.5 * boxA[:,:,:,:,2], boxB[:,:,:,:,0] - 0.5 * boxA[:,:,:,:,2])
+    yB = tf.minimum(boxA[:,:,:,:,0] + 0.5 * boxA[:,:,:,:,2], boxB[:,:,:,:,0] + 0.5 * boxB[:,:,:,:,2])
+
+    xA = tf.maximum(boxA[:,:,:,:,1] - 0.5 * boxA[:,:,:,:,3], boxB[:,:,:,:,1] - 0.5 * boxB[:,:,:,:,3])
+    xB = tf.minimum(boxA[:,:,:,:,1] + 0.5 * boxA[:,:,:,:,3], boxB[:,:,:,:,1] + 0.5 * boxB[:,:,:,:,3])
+
+    # compute the area of intersection rectangle
+    iy = yB - yA
+    ix = xB - xA
+    interArea = tf.maximum(tf.zeros_like(iy), iy) * tf.maximum(tf.zeros_like(ix), ix)
+
+    # compute the area of both the prediction and ground-truth rectangles
+    boxAArea = tf.abs(boxA[:,:,:,:,2] * boxA[:,:,:,:,3])
+    boxBArea = tf.abs(boxB[:,:,:,:,2] * boxB[:,:,:,:,3])
+
+    # compute the intersection over union by taking the intersection
+    # area and dividing it by the sum of prediction + ground-truth
+    # areas - the interesection area
+    iou = interArea / (boxAArea + boxBArea - interArea)
+
+    # return the intersection over union value
+    return iou
+
+@tf.custom_gradient
+def abs_no_grad(x):
+    def grad(dy):
+        return dy
+    return tf.abs(x), grad
+
+@tf.custom_gradient
+def sign_no_grad(x):
+    def grad(dy):
+        return dy
+    return tf.sign(x), grad
+
+def yolo_loss(pred, label, obj, no_obj, cat, vld):
+
+    # pred   = [4,     7, 7, 90]
+    # label  = [4, -1, 7, 7, 5]
+    # obj    = [4, -1, 7, 7]
+    # no_obj = [4, -1, 7, 7]
+    # cat    = [4, -1, 7, 7]
+
+    pred = tf.reshape(pred, [8, 1, 5, 6, 12])
+
+    ############################
+
+    label_box = grid_to_pix(label[:, :, :, :, 0:4])
+    pred_box1 = grid_to_pix(pred[:, :, :, :, 0:4])
+    pred_box2 = grid_to_pix(pred[:, :, :, :, 5:9])
+
+    iou = calc_iou(pred_box1, pred_box2, label_box)
+    resp_box = tf.greater(iou[:, :, :, :, 0], iou[:, :, :, :, 1])
+
+    ######################################
+
+    label_yx = label[:, :, :, :, 0:2]
+    pred_yx1 = pred[:, :, :, :, 0:2]
+    pred_yx2 = pred[:, :, :, :, 5:7]
+
+    loss_yx1 = tf.reduce_sum(tf.square(pred_yx1 - label_yx), axis=4)
+    loss_yx2 = tf.reduce_sum(tf.square(pred_yx2 - label_yx), axis=4)
+    yx_loss = 5. * obj * vld * tf.where(resp_box, loss_yx1, loss_yx2)
+    yx_loss = tf.reduce_mean(tf.reduce_sum(yx_loss, axis=[2, 3]))
+
+    ######################################
+
+    # label_hw = tf.sqrt(label[:, :, :, :, 2:4])
+    # pred_hw1 = tf.sqrt(abs_no_grad(pred[:, :, :, :, 2:4])) * sign_no_grad(pred[:, :, :, :, 2:4])
+    # pred_hw2 = tf.sqrt(abs_no_grad(pred[:, :, :, :, 7:9])) * sign_no_grad(pred[:, :, :, :, 7:9])
+
+    label_hw = label[:, :, :, :, 2:4]
+    pred_hw1 = pred[:, :, :, :, 2:4]
+    pred_hw2 = pred[:, :, :, :, 7:9]
+
+    loss_hw1 = tf.reduce_sum(tf.square(pred_hw1 - label_hw), axis=4)
+    loss_hw2 = tf.reduce_sum(tf.square(pred_hw2 - label_hw), axis=4)
+    hw_loss = 5. * obj * vld * tf.where(resp_box, loss_hw1, loss_hw2)
+    hw_loss = tf.reduce_mean(tf.reduce_sum(hw_loss, axis=[2, 3]))
+
+    ######################################
+
+    label_conf = label[:, :, :, :, 4]
+    pred_conf1 = pred[:, :, :, :, 4]
+    pred_conf2 = pred[:, :, :, :, 9]
+
+    loss_obj1 = tf.square(pred_conf1 - label_conf)
+    loss_obj2 = tf.square(pred_conf2 - label_conf)
+    obj_loss = 1. * obj * vld * tf.where(resp_box, loss_obj1, loss_obj2)
+    obj_loss = tf.reduce_mean(tf.reduce_sum(obj_loss, axis=[2, 3]))
+
+    loss_no_obj1 = tf.square(pred_conf1 - label_conf)
+    loss_no_obj2 = tf.square(pred_conf2 - label_conf)
+    no_obj_loss = 0.5 * no_obj * vld * tf.where(resp_box, loss_no_obj1, loss_no_obj2)
+    no_obj_loss = tf.reduce_mean(tf.reduce_sum(no_obj_loss, axis=[2, 3]))
+
+    ######################################
+
+    label_cat = tf.one_hot(cat, depth=2)
+    pred_cat = pred[:, :, :, :, 10:12]
+
+    cat_loss = 2. * obj * vld * tf.reduce_sum(tf.square(pred_cat - label_cat), axis=4)
+    cat_loss = tf.reduce_mean(tf.reduce_sum(cat_loss, axis=[2, 3]))
+
+    ######################################
+
+    loss = yx_loss + hw_loss + obj_loss + no_obj_loss + cat_loss
+    return loss
+
+
+
+
+
+
+
+
+
