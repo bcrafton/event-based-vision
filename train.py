55d52d5f2032164ce5fc1eca5a40d9fe6d1d28bf
Brian A Crafton <bcrafton3@KL2125-01.ece.gatech.edu>
2020-07-12 12:33:32 -0400

    new model gets 0.2 mAP on validation

--- train.py
+++ train.py
@@ -47,10 +47,11 @@ from calc_map import calc_map
 ####################################
 
 if args.train:
-    # weights = None # weights = np.load('resnet18.npy', allow_pickle=True).item()
-    weights = np.load('models/small_resnet_yolo_abcdef.npy', allow_pickle=True).item()
+    weights = np.load('models/resnet_yolo.npy', allow_pickle=True).item()
+    dropout = True
 else:
-    weights = np.load('models/small_resnet_yolo_abcdef.npy', allow_pickle=True).item()
+    weights = np.load('models/resnet_yolo.npy', allow_pickle=True).item()
+    dropout = False
 
 ####################################
 
@@ -61,32 +62,30 @@ conv_block((7,7,12,64), 1, weights=weights), # 240, 288
 max_pool(s=3, p=3),
 
 res_block1(64,   64, 1, weights=weights), # 80, 96
-# res_block1(64,   64, 1, weights=weights), # 80, 96
+res_block1(64,   64, 1, weights=weights), # 80, 96
 
 max_pool(s=2, p=2),
 
 res_block2(64,   128, 1, weights=weights), # 40, 48
-# res_block1(128,  128, 1, weights=weights), # 40, 48
+res_block1(128,  128, 1, weights=weights), # 40, 48
 
 max_pool(s=2, p=2),
 
 res_block2(128,  256, 1, weights=weights), # 20, 24
-# res_block1(256,  256, 1, weights=weights), # 20, 24
+res_block1(256,  256, 1, weights=weights), # 20, 24
 
 max_pool(s=2, p=2),
 
 res_block2(256,  512, 1, weights=weights), # 10, 12
-# res_block1(512,  512, 1, weights=weights), # 10, 12
+res_block1(512,  512, 1, weights=weights), # 10, 12
 
 max_pool(s=2, p=2),
 
-# res_block2(512,  512, 1, weights=weights), # 5, 6
-# res_block1(512,  512, 1, weights=weights), # 5, 6
+res_block2(512,  512, 1, weights=weights), # 5, 6
+res_block1(512,  512, 1, weights=weights), # 5, 6
 
-# dense_block(5*6*512, 1024, weights=weights),
-# dense_block(1024, 5*6*12, weights=weights, relu=False),
-
-dense_block(5*6*512, 5*6*12, weights=weights, relu=False),
+dense_block(5*6*512, 2048, weights=weights, dropout=dropout),
+dense_block(2048, 5*6*12, weights=weights, relu=False),
 ])
 
 params = model.get_params()
